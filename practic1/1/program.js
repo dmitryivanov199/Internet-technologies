48c7e113eda05306c2b4927b7f8704da2988e62f
dmitryivanov199 <dmitryivanov199@mail.ru>
2021-10-14 18:38:17 +0300

    Add file .pdf with tasks and completed tasks on arrays

---
+++ practic1/1/program.js
@@ -0,0 +1,39 @@
+function make1_1(A, B) {
+  return A.concat(B);
+}
+
+function make1_3(array) {
+  max = array[0];
+
+  for(i = 0; i < array.length; i++) {
+    if(array[i] > max) {
+      max = array[i];
+    }
+  }
+
+  min = array[0];
+
+  for(i = 0; i < array.length; i++) {
+    if(array[i] < min) {
+      min = array[i];
+    }
+  }
+
+  array.splice(array.indexOf(min), 1);
+  array.splice(array.indexOf(max), 1);
+  return array;
+}
+
+A = [1, 2, 3];
+B = [4, 5, 6];
+console.log("Task 1.1");
+console.log("A = " + A);
+console.log("B = " + B);
+console.log("Result = " + make1_1(A, B));
+
+console.log("");
+
+array = [12, 4, 3, 10, 1, 20];
+console.log("Task 1.3");
+console.log("array = " + array);
+console.log("Result = " + make1_3(array));
